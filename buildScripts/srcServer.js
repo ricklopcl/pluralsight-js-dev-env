7f0216e9e6424f8af6883000521fb9941b040f1e
Ricardo Lopez Castillo <ricklop.cl@gmail.com>
2018-05-10 14:16:12 -0400

    Added npm scripts to run app

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -6,13 +6,13 @@ var port = 3000;
 var app = express();
 
 app.get('/', function(request, response) {
-  response.sendFile(path.join(__dirname, '.../src/index.html'));
+  response.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
 app.listen(port, function(error) {
   if(error) {
     console.log(error);
   } else {
-    open('http//localhost:' + port);
+    open('http://localhost:' + port);
   }
 });
